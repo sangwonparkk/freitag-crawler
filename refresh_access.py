827591d22205e0af0b30b2a1132b72eebdbc9494
Sangwon Park <18sp01@gmail.com>
2021-07-18 23:01:50 +0900

    Changed files to use threading instead of scheduler
    Currently calls to website every 2 minutes

--- refresh_access.py
+++ refresh_access.py
@@ -2,6 +2,7 @@ import json
 import requests
 import schedule
 import time
+import threading
 
 url = "https://kauth.kakao.com/oauth/token"
 
@@ -10,22 +11,18 @@ data = {
     "client_id": "bf46c47a18a2e1e0679ee3a14fe49ab3",
     "refresh_token": "t9XTb0Bez9OxQvDSZKTlkaEFiixmDoEX2p1WRQo9cusAAAF6ucJgfA"
 }
-response = requests.post(url, data=data)
 
 
 def job():
-    response = requests.post(url, data=data)
+    r = requests.post(url, data=data)
     print("Refreshed")
+    threading.Timer(19800, job).start()
+    return r
 
 
-schedule.every(5).hours.do(job)
-
+response = job()
 tokens = response.json()
 
 # kakao_code.json 파일 저장
 with open("kakao_code.json", "w") as fp:
     json.dump(tokens, fp)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
